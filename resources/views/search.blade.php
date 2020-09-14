0710c246defe5fa17a00034c142c46435c71a4ce
tareq <taregh1996nazari@gmail.com>
2020-09-14 09:09:24 -0700

    'asd'

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -29,27 +29,32 @@
                     </div>
                     <div class="advance-search">
                         <span class="desc">جستجوی پیشرفته</span>
-                        <div style="display: grid;grid-template-columns: 1fr">
 
-                            <div style="display: grid;grid-template-columns: 1fr 1fr 1fr">
+
+                            <div style="" class="row first">
                                 <div class="input-field">
-                                    <div class="input-select">انتخاب ژانرفیلم :
-                                        <select data-trigger="" name="janre">
-                                            @foreach($cats as $cat)
-                                                <option value="{{$cat->name}}">{{$cat->name}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
+                                    <button type="button" class="btn btn-warning" style="" onclick="showJanre()">انتخاب ژانر</button>
                                 </div>
-                                <div  style="display: grid;grid-template-columns: 1fr "  class="input-field">
+                                <div  style=""  class="input-field">
                                     <label style="color: white;margin-bottom: 2px">سال ساخت : </label>
-                                    <input style="height: 50px;width: 100px" placeholder="مثال 1998" type="text" name="p_year">
+                                    <input style="height: 100%;width: 50%;padding: 10px"  placeholder="مثال 1998" type="text" name="p_year">
                                 </div>
-                                <div   style="display: grid;grid-template-columns: 1fr " class="input-field">
-                                    <label style="color: white;margin-bottom: 2px"> محصول : </label>
-                                    <input style="height: 50px;width: 100px" type="text" name="product">
+                                <div   style="" class="input-field">
+                                    <label style="color: white;margin-bottom: 2px"> کشور : </label>
+                                    <input style="height: 100%;width: 50%;padding: 10px" type="text" name="product">
                                 </div>
                             </div>
+                        <div id="janre" style="visibility: hidden;width: 100%;background-color: white;padding: 5px;display: flex;flex-wrap: wrap">
+                            @foreach($cats as $cat)
+                           <label style="margin: 0 20px">{{$cat->name}}
+                               <input class="" type="checkbox">
+                           </label>
+
+                            @endforeach
+
+                        </div>
+
+                        <br>
                             <div class="row second">
                                 <div class="input-field" style="width: 67px">
                                     <label style="color: white;margin-bottom: 2px">امتیاز : </label>
@@ -79,11 +84,32 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+
                     </div>
             </form>
 
         </div>
 
     </div>
+    <div style="background-color: white;width: 100%;display: flex;flex-wrap: wrap;padding: 20px">
+        @for($i=0;$i<10;$i++)
+            <div class="card" style="width: 18rem;margin: 25px">
+                <img class="card-img-top" style="height: 150px;width: 150px" src="{{asset('images/baelen.jpg')}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-danger">مشاهده فیلم</a>
+                </div>
+            </div>
+        @endfor
+    </div>
+    <script>
+       function showJanre(){
+
+            let x = document.getElementById('janre')
+           if(x.style.visibility === 'hidden'){
+               x.style.visibility = 'visible'
+           } else x.style.visibility = 'hidden'
+        }
+    </script>
 @endsection
